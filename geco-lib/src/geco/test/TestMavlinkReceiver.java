1f5ac1445977a5d09e3085cdd07b8e75d311f0c9
Tom Langfeldt <tom.langfeldt@sigfox.com>
2016-08-30 18:20:11 +0200

    commit du soir

--- geco-lib/src/geco/test/TestMavlinkReceiver.java
+++ geco-lib/src/geco/test/TestMavlinkReceiver.java
@@ -1,24 +1,36 @@
 package geco.test;
 
 import com.MAVLink.Messages.MAVLinkMessage;
+import com.MAVLink.common.msg_debug;
+import com.MAVLink.common.msg_global_position_int;
 import com.MAVLink.common.msg_heartbeat;
+import com.MAVLink.common.msg_radio_status;
+import com.MAVLink.common.msg_statustext;
+import com.MAVLink.common.msg_terrain_data;
 
 import geco.io.IDataConnector;
 import geco.io.MavlinkMessageReceiver;
 
 public class TestMavlinkReceiver extends MavlinkMessageReceiver
 {
-
+	private boolean m_DroneConnected = false;
+	
+	public void onConnected()
+		{
+			this.m_DroneConnected = true;
+			System.out.println("drone connected");
+		}
+	
 	@Override
 	public void onMessageReceived(MAVLinkMessage p_Message) 
 	{
-		//System.out.println(String.valueOf(p_Message.sysid) + " : " + p_Message);
-		
 		if (p_Message.getClass() == msg_heartbeat.class)
 			{
 				System.out.println(String.valueOf(p_Message.sysid) + " : " + p_Message);
+				
+				if (!this.m_DroneConnected)
+					this.onConnected();
 			}
-		
 	}
 
 	@Override
